89f087519deb968a78b9482aeb6ca5c533356e09
Jay Vora <marioojojo@gmail.com>
2026-06-10 00:00:54 +0530

    Resume updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,7 +18,7 @@ export const SITE_CONFIG: SiteConfig = {
     { text: "LinkedIn", href: "https://www.linkedin.com/in/marioojojo" },
     //{ text: "Github", href: "https://github.com/marioojojo13" },
     //{ text: "Resume", href: "https://drive.google.com/file/d/1fWNt4Ah04oKQGmXZTSZLce9MPR-_rZZg/view?usp=drive_link" },
-    { text: "Resume", href: "https://drive.google.com/file/d/1EaFlo_IyfrdLcIkAW2YNUrPzbJTjmoTF/view?usp=sharing" },
+    { text: "Resume", href: "https://drive.google.com/file/d/1ITjh8_scVDQV6XQepn7rpjsWVgMLrlq8/view?usp=sharing" },
   ],
   socialImage: "/jay-img.jpg",
   canonicalURL: "https://jay-vora.vercel.app",
